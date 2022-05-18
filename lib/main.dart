89bd9d10804efd3a872658dd9a14bf16699a3f58
Athola Bahizire <atholabahizire@gmail.com>
2022-05-18 23:23:45 +0200

    add CurvedNavigation and Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
 void main(){
   runApp(SqliteApp());
@@ -27,23 +28,75 @@ class _SqliteAppState extends State<SqliteApp> {
         body: Center(
           child: Text("Selected Item: $_selectedIndex"),
         ),
-        bottomNavigationBar: BottomNavigationBar(
+        bottomNavigationBar:
+        // BottomNavigationBar(
+        //   items: [
+        //     BottomNavigationBarItem(icon: Icon(Icons.business),label: "Business"),
+        //     BottomNavigationBarItem(icon: Icon(Icons.school),label: "School"),
+        //     BottomNavigationBarItem(icon: Icon(Icons.account_circle),label: "Account"),
+        //   ],
+        //   currentIndex: _selectedIndex,
+        //   onTap: _onItemTapped,
+        //   unselectedIconTheme: IconThemeData( size: 30),
+        //   showUnselectedLabels: false,
+        // ),
+        CurvedNavigationBar(
           items: [
-            BottomNavigationBarItem(icon: Icon(Icons.business),label: "Business"),
-            BottomNavigationBarItem(icon: Icon(Icons.school),label: "School"),
-            BottomNavigationBarItem(icon: Icon(Icons.account_circle),label: "Account"),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Icon(Icons.business, color: Colors.white,),
+                  Text("Business", style: TextStyle(color: Colors.white),)
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Icon(Icons.school,color: Colors.white,),
+                  Text("School", style: TextStyle(color: Colors.white),)
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Icon(Icons.link_sharp,color: Colors.white,),
+                  Text("Link", style: TextStyle(color: Colors.white),)
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Icon(Icons.account_circle,color: Colors.white,),
+                  Text("Account", style: TextStyle(color: Colors.white),)
+                ],
+              ),
+            ),
           ],
-          currentIndex: _selectedIndex,
+          color: Colors.blueAccent,
+          backgroundColor: Colors.white,
+          height: 60,
+          index: _selectedIndex,
           onTap: _onItemTapped,
-          unselectedIconTheme: IconThemeData( size: 30),
-          showUnselectedLabels: false,
-
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: (){
-            print("Oklm");
-          },
-          child: Icon(Icons.add_a_photo_outlined),
+        // floatingActionButton: FloatingActionButton(
+        //   onPressed: (){
+        //     print("Oklm");
+        //   },
+        //   child: Icon(Icons.add_a_photo_outlined),
+        // ),
+        drawer: Drawer(
+
         ),
       ),
     );

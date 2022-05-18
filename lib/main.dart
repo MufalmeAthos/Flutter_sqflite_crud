2546bb81280d4a82db4cc22145c2130cd2883aca
Athola Bahizire <atholabahizire@gmail.com>
2022-05-18 23:04:27 +0200

    add bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,38 @@ class SqliteApp extends StatefulWidget {
 }
 
 class _SqliteAppState extends State<SqliteApp> {
+  int _selectedIndex = 0;
+  void _onItemTapped(int index){
+    // print("index: $index");
+    setState((){
+      _selectedIndex = index;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text("Sqlite Crud App"),),
         body: Center(
-          child: Text("No text yet!"),
+          child: Text("Selected Item: $_selectedIndex"),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.business),label: "Business"),
+            BottomNavigationBarItem(icon: Icon(Icons.school),label: "School"),
+            BottomNavigationBarItem(icon: Icon(Icons.account_circle),label: "Account"),
+          ],
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          unselectedIconTheme: IconThemeData( size: 30),
+          showUnselectedLabels: false,
+
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            print("Oklm");
+          },
+          child: Icon(Icons.add_a_photo_outlined),
         ),
       ),
     );

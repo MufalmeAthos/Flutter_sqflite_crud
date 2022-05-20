caeee56f0f64f39d59d1dc9957930d973659f371
Athola Bahizire <atholabahizire@gmail.com>
2022-05-20 14:50:01 +0200

    add drawer menus and textfield on modal

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:sqlitecrud/modal.dart';
@@ -14,146 +17,287 @@ class SqliteApp extends StatefulWidget {
 }
 
 class _SqliteAppState extends State<SqliteApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
   int _selectedIndex = 0;
   void _onItemTapped(int index){
     // print("index: $index");
     if(index==3){
-      Navigator.push(context, MaterialPageRoute(builder: (context){ return  const MyApp();} ));
+      Navigator.push(context, MaterialPageRoute(builder: (context){ return  const MyStatelessWidget();} ));
     }
     setState((){
       _selectedIndex = index;
     });
   }
+   TextEditingController _textController = new TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("Sqlite Crud App"),),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-              child: Text("Selected Item: $_selectedIndex"),
-            ),
-            SizedBox(height: 20,),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children:  [
-                InkWell(
-                  onTap: (){
-                    print("$context");
-                    showModalBottomSheet(
-                        context: context,
-                        builder: (context)=>Container(
-                          height: 200,
-                            child: Text("oklm"),
-                        )
-                    );
-                  },
-                  child: Card(
-
-                    child: Padding(
-                      padding: EdgeInsets.all(20.0),
-                      child: Text("Modal Bottom Sheet"),
-                    ),
+    return Scaffold(
+      appBar: AppBar(title: Text("Sqlite Crud App"),),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text("Selected Item: $_selectedIndex"),
+          ),
+          SizedBox(height: 20,),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children:  [
+              InkWell(
+                onTap: (){
+                  _modalSheet(context);
+                },
+                child: Card(
+                  child: Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Text("Modal Bottom Sheet"),
                   ),
                 ),
-                InkWell(
-                  onTap: (){
-                    print("persistent");
-                    Navigator.push(context, MaterialPageRoute(builder: (context){ return  MyApp();} ));
-                  },
-                  child: Card(
-                    child: Padding(
-                      padding: EdgeInsets.all(20.0),
-                      child: Text("Persistent Bottom Sheet"),
-                    ),
+              ),
+              InkWell(
+                onTap: (){
+                  print("persistent");
+                },
+                child: Card(
+                  child: Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Text("Persistent Bottom Sheet"),
                   ),
-                )
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+      bottomNavigationBar:
+      // BottomNavigationBar(
+      //   items: [
+      //     BottomNavigationBarItem(icon: Icon(Icons.business),label: "Business"),
+      //     BottomNavigationBarItem(icon: Icon(Icons.school),label: "School"),
+      //     BottomNavigationBarItem(icon: Icon(Icons.account_circle),label: "Account"),
+      //   ],
+      //   currentIndex: _selectedIndex,
+      //   onTap: _onItemTapped,
+      //   unselectedIconTheme: IconThemeData( size: 30),
+      //   showUnselectedLabels: false,
+      // ),
+      CurvedNavigationBar(
+        items: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Icon(Icons.business, color: Colors.white,),
+                Text("Business", style: TextStyle(color: Colors.white),)
               ],
-            )
-          ],
-        ),
-        bottomNavigationBar:
-        // BottomNavigationBar(
-        //   items: [
-        //     BottomNavigationBarItem(icon: Icon(Icons.business),label: "Business"),
-        //     BottomNavigationBarItem(icon: Icon(Icons.school),label: "School"),
-        //     BottomNavigationBarItem(icon: Icon(Icons.account_circle),label: "Account"),
-        //   ],
-        //   currentIndex: _selectedIndex,
-        //   onTap: _onItemTapped,
-        //   unselectedIconTheme: IconThemeData( size: 30),
-        //   showUnselectedLabels: false,
-        // ),
-        CurvedNavigationBar(
-          items: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Icon(Icons.business, color: Colors.white,),
-                  Text("Business", style: TextStyle(color: Colors.white),)
-                ],
-              ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Icon(Icons.school,color: Colors.white,),
-                  Text("School", style: TextStyle(color: Colors.white),)
-                ],
-              ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Icon(Icons.school,color: Colors.white,),
+                Text("School", style: TextStyle(color: Colors.white),)
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Icon(Icons.link_sharp,color: Colors.white,),
-                  Text("Link", style: TextStyle(color: Colors.white),)
-                ],
-              ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Icon(Icons.link_sharp,color: Colors.white,),
+                Text("Link", style: TextStyle(color: Colors.white),)
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Icon(Icons.account_circle,color: Colors.white,),
-                  Text("Account", style: TextStyle(color: Colors.white),)
-                ],
-              ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Icon(Icons.account_circle,color: Colors.white,),
+                Text("Account", style: TextStyle(color: Colors.white),)
+              ],
             ),
-          ],
-          color: Colors.blueAccent,
-          backgroundColor: Colors.white,
-          height: 60,
-          index: _selectedIndex,
-          onTap: _onItemTapped,
-        ),
-        // floatingActionButton: FloatingActionButton(
-        //   onPressed: (){
-        //     print("Oklm");
-        //   },
-        //   child: Icon(Icons.add_a_photo_outlined),
-        // ),
-        // bottomSheet: BottomSheet(onClosing: (){}, builder: ,),
-        drawer: Drawer(
+          ),
+        ],
+        color: Colors.blueAccent,
+        backgroundColor: Colors.white,
+        height: 60,
+        index: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+      floatingActionButton: FloatingActionButton(
+        // isExtended: true,
+        // tooltip: "oklm",
 
+        onPressed: (){
+          print("Oklm");
+          _datePicker(context);
+          // Navigator.of(context).push(MaterialPageRoute(builder: (context){return MyStatelessWidget();}));
+        },
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.date_range_outlined),
+            // Text(" data"),
+          ],
+        ),
+      ),
+      drawer: Drawer(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: ListView(
+            children: [
+              Text("Main Menu"),
+              SizedBox(height: 20,),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: GestureDetector(
+                  onTap: (){
+                    Navigator.push( context,MaterialPageRoute(
+                        builder: (context) =>  MyHomePage()
+                    ));
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(Icons.business, color: Colors.blueAccent,),
+                      SizedBox(width: 15,),
+                      Text("Business", style: TextStyle(color: Colors.black),)
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Icon(Icons.school,color: Colors.blueAccent,),
+                    SizedBox(width: 15,),
+                    Text("School", style: TextStyle(color: Colors.black),)
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Icon(Icons.link_sharp,color: Colors.blueAccent,),
+                    SizedBox(width: 15,),
+                    Text("Link", style: TextStyle(color: Colors.black),)
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: GestureDetector(
+                  onTap: (){
+                    Navigator.push(context, MaterialPageRoute(
+                        builder: (context)=> MyStatelessWidget()
+                    ));
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(Icons.account_circle,color: Colors.blueAccent,),
+                      SizedBox(width: 15,),
+                      Text("Account", style: TextStyle(color: Colors.black),)
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
-
   }
-  Widget _modalSheet(BuildContext context){
-    return Container(
-      height: 200,
-      // color: ,
-    );
 
+
+  Widget? _modalSheet(BuildContext context){
+    showModalBottomSheet(
+      // shape: ShapeBorder(),
+        context: context,
+        builder: (context) {
+          return Container(
+            height: 200,
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Icon(Icons.arrow_drop_down),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Enter Task"),
+                    TextField(
+                      controller: _textController,
+
+                    ),
+                    ElevatedButton(
+                        onPressed: (){
+                          print(_textController.text);
+                          _textController.clear();
+                          Timer(
+                            Duration(milliseconds: 500),
+                            () {
+                              Navigator.pop(context);
+                            },
+                          );
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.save_alt),
+                            Text(" Save")
+                          ],
+                        ),
+                    ),
+                  ],
+                ),
+                IconButton(
+                    onPressed: (){
+                      Navigator.of(context).pop();
+                    },
+                    icon: Icon(Icons.close_sharp)
+                ),
+              ],
+            ),
+          );
+        }
+ );
+  }
+
+  Widget? _datePicker(BuildContext context){
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now() ,
+      firstDate: DateTime(1990),
+      lastDate: DateTime(2050),
+    );
   }
 }
+
+
--- lib/modal.dart
+++ lib/modal.dart
@@ -1,53 +1,177 @@
 import 'package:flutter/material.dart';
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:sqlitecrud/main.dart';
 
-  static const String _title = 'Flutter Code Sample';
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: _title,
-      home: Scaffold(
-        appBar: AppBar(title: const Text(_title)),
-        body: const MyStatelessWidget(),
-      ),
-    );
-  }
-}
-
-class MyStatelessWidget extends StatelessWidget {
+class MyStatelessWidget extends StatefulWidget {
   const MyStatelessWidget({Key? key}) : super(key: key);
 
   @override
+  State<MyStatelessWidget> createState() => _MyStatelessWidgetState();
+}
+class _MyStatelessWidgetState extends State<MyStatelessWidget> {
+
+  int _selectedIndex = 3;
+  void _onItemTapped(int index){
+    // print("index: $index");
+
+    if(index==0){
+      Navigator.push(context, MaterialPageRoute(builder: (context){ return  const MyHomePage();} ));
+    }
+    setState((){
+      _selectedIndex = index;
+    });
+  }
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ElevatedButton(
-        child: const Text('showModalBottomSheet'),
-        onPressed: () {
-          showModalBottomSheet<void>(
-            context: context,
-            builder: (BuildContext context) {
-              return Container(
-                height: 200,
-                color: Colors.amber,
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      const Text('Modal BottomSheet'),
-                      ElevatedButton(
-                        child: const Text('Close BottomSheet'),
-                        onPressed: () => Navigator.pop(context),
-                      )
+    return Scaffold(
+      appBar: AppBar(title: Text("Sqlite Crud App"),),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('showModalBottomSheet'),
+          onPressed: () {
+            showModalBottomSheet<void>(
+              context: context,
+              builder: (BuildContext context) {
+                return Container(
+                  height: 200,
+                  color: Colors.white,
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        const Text('Modal BottomSheet'),
+                        ElevatedButton(
+                          child: const Text('Close BottomSheet'),
+                          onPressed: () => Navigator.pop(context),
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          },
+        ),
+      ),
+      bottomNavigationBar:
+      CurvedNavigationBar(
+        items: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Icon(Icons.business, color: Colors.white,),
+                Text("Business", style: TextStyle(color: Colors.white),)
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Icon(Icons.school,color: Colors.white,),
+                Text("School", style: TextStyle(color: Colors.white),)
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Icon(Icons.link_sharp,color: Colors.white,),
+                Text("Link", style: TextStyle(color: Colors.white),)
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Icon(Icons.account_circle,color: Colors.white,),
+                Text("Account", style: TextStyle(color: Colors.white),)
+              ],
+            ),
+          ),
+        ],
+        color: Colors.blueAccent,
+        backgroundColor: Colors.white,
+        height: 60,
+        index: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+      drawer: Drawer(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: ListView(
+            children: [
+              Text("Main Menu"),
+              SizedBox(height: 20,),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: GestureDetector(
+                  onTap: (){
+                    Navigator.push( context,MaterialPageRoute(
+                        builder: (context) =>  MyHomePage()
+                    ));
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(Icons.business, color: Colors.blueAccent,),
+                      SizedBox(width: 15,),
+                      Text("Business", style: TextStyle(color: Colors.black),)
                     ],
                   ),
                 ),
-              );
-            },
-          );
-        },
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Icon(Icons.school,color: Colors.blueAccent,),
+                    SizedBox(width: 15,),
+                    Text("School", style: TextStyle(color: Colors.black),)
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Icon(Icons.link_sharp,color: Colors.blueAccent,),
+                    SizedBox(width: 15,),
+                    Text("Link", style: TextStyle(color: Colors.black),)
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: GestureDetector(
+                  onTap: (){
+                    Navigator.push(context, MaterialPageRoute(
+                        builder: (context)=> MyStatelessWidget()
+                    ));
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(Icons.account_circle,color: Colors.blueAccent,),
+                      SizedBox(width: 15,),
+                      Text("Account", style: TextStyle(color: Colors.black),)
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }

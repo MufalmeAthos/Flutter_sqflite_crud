97d560a2364f0cf26cc619d3a9d1af15d718e31d
Athola Bahizire <atholabahizire@gmail.com>
2022-05-22 17:27:30 +0200

    add new task and display tasks from db

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'dart:async';
+import 'dart:io';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
 import 'package:sqlitecrud/modal.dart';
 
 void main(){
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(SqliteApp());
 }
 
@@ -35,10 +40,11 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
   int _selectedIndex = 0;
+  BuildContext? context_ ;
   void _onItemTapped(int index){
     // print("index: $index");
     if(index==3){
-      Navigator.push(context, MaterialPageRoute(builder: (context){ return  const MyStatelessWidget();} ));
+      Navigator.push(context_!, MaterialPageRoute(builder: (context){ return  const MyStatelessWidget();} ));
     }
     setState((){
       _selectedIndex = index;
@@ -46,47 +52,85 @@ class _MyHomePageState extends State<MyHomePage> {
   }
   TextEditingController _textController = new TextEditingController();
   final _scafoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
+    context_ = context;
     return Scaffold(
       key: _scafoldKey,
       appBar: AppBar(title: Text("Sqlite Crud App"),),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Text("Selected Item: $_selectedIndex"),
-          ),
-          SizedBox(height: 20,),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children:  [
-              InkWell(
-                onTap: (){
-                  _modalSheet(context);
-                },
-                child: Card(
-                  child: Padding(
-                    padding: EdgeInsets.all(20.0),
-                    child: Text("Modal Bottom Sheet"),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(vertical: 30, horizontal: 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+
+          children: [
+            Center(
+              child: Text("Selected Item: $_selectedIndex"),
+            ),
+            SizedBox(height: 20,),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children:  [
+                InkWell(
+                  onTap: (){
+                    _modalSheet(context);
+                  },
+                  child: Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: Text("Modal Bottom Sheet"),
+                    ),
                   ),
                 ),
+                InkWell(
+                  onTap: (){
+                    print("persistent");
+                    _persistentSheet(context);
+                  },
+                  child: Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: Text("Persistent Bottom Sheet"),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 20,),
+            Text("Tasks of the day", style: TextStyle(color: Colors.blueAccent,fontSize: 14), ),
+            Text("(Click the 'Modal Sheet button' to add a task)", style: TextStyle(fontSize: 12),),
+
+
+            /** The List items now **/
+            Center(
+              child: FutureBuilder<List<TaskList>>(
+                future: DatabaseHelper.instance.getTasks(),
+                builder: (BuildContext context, AsyncSnapshot<List<TaskList>> snapshot){
+
+                  // print(snapshot.data);
+                  if(!snapshot.hasData){
+                    return Center(child: Text("Loading..."),);
+                  }
+                  return snapshot.data!.isEmpty
+                    ?Center(child: Text("No data yet!"),)
+                    :Column(
+                    children: snapshot.data!.map((taskList) {
+                      return Center(
+                        child: ListTile(
+                          title: Text("- "+taskList.name),
+                        ),
+                      );
+                    }).toList(),
+                  );
+                },
+
               ),
-              InkWell(
-                onTap: (){
-                  print("persistent");
-                  _persistentSheet(context);
-                },
-                child: Card(
-                  child: Padding(
-                    padding: EdgeInsets.all(20.0),
-                    child: Text("Persistent Bottom Sheet"),
-                  ),
-                ),
-              )
-            ],
-          )
-        ],
+            )
+
+          ],
+        ),
       ),
       bottomNavigationBar:
       // BottomNavigationBar(
@@ -263,19 +307,25 @@ class _MyHomePageState extends State<MyHomePage> {
                     Text("New Task"),
                     TextField(
                       decoration: InputDecoration(
-                        label: Text("Enter a task")
+                        label: Text("Enter a task"),
+                        icon: Icon(Icons.text_fields),
                       ),
                       controller: _textController,
 
                     ),
                     ElevatedButton(
-                      onPressed: (){
+                      onPressed: () async{
+                        await DatabaseHelper.instance.add(
+                          TaskList(name: _textController.text),
+                        );
                         print(_textController.text);
                         _textController.clear();
                         Timer(
                           Duration(milliseconds: 500),
-                              () {
-                            Navigator.pop(context);
+                          () {
+                            Navigator.push(context, MaterialPageRoute(
+                                builder: (context){ return MyHomePage();}
+                            ));
                           },
                         );
                       },
@@ -349,5 +399,68 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+
+class TaskList{
+  final int? id;
+  final String name;
+  TaskList({this.id, required this.name});
+  factory  TaskList.fromMap(Map<String, dynamic> json) =>  TaskList(
+      id: json['id'],
+      name: json['name']
+  );
+
+  Map<String, dynamic> toMap(){
+    return {
+      "id": id,
+      "name": name,
+    };
+  }
+
+}
+
+class DatabaseHelper{
+  DatabaseHelper._privateConstructor();
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+  static Database? _database;
+  Future<Database> get database async => _database ??= await _initDatabase();
+
+  Future<Database> _initDatabase() async{
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, 'tasklist.db');
+    // print("object");
+    // print(path);
+    // print(join(documentsDirectory.path, 'tasklist.db'));
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _onCreate,
+    );
+  }
+  Future _onCreate(Database db, int version) async{
+    await db.execute('''
+    CREATE TABLE tasks(
+    id INTEGER PRIMARY KEY,
+    name TEXT
+    )
+    ''');
+  }
+  Future<List<TaskList>> getTasks() async{
+    Database db = await instance.database;
+    // print("object1");
+    // print(db);
+    var tasks = await db.query('tasks',orderBy: 'id');
+    List<TaskList> taskList = tasks.isNotEmpty
+    ? tasks.map( (c)=> TaskList.fromMap(c) ).toList()
+    : [];
+    return taskList;
+
+  }
+  Future add(TaskList taskList) async{
+    Database db = await instance.database;
+    return await db.insert('tasks', taskList.toMap());
+  }
+}
+
 
 
